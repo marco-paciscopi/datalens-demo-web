10e4a21a772b7fbfedde4df1392aab1e0304f98a
pfinardiIts <paolo.finardi@enigaseluce.com>
2023-03-06 19:08:59 +0100

    update from disk

--- ocr_plenitude.py
+++ ocr_plenitude.py
@@ -11,15 +11,16 @@ api_key = st.secrets["API_KEY"]
 api_url = st.secrets["API_URL"]
 
 def displayPDF(file):
-    # Opening file from file path
-    with open(file, "rb") as f:
-        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+    with col1:
+        # Opening file from file path
+        with open(file, "rb") as f:
+            base64_pdf = base64.b64encode(f.read()).decode('utf-8')
 
-    # Embedding PDF in HTML
-    pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
+        # Embedding PDF in HTML
+        pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
 
-    # Displaying File
-    st.markdown(pdf_display, unsafe_allow_html=True)
+        # Displaying File
+        st.markdown(pdf_display, unsafe_allow_html=True)
 
 
 st.set_page_config(layout="wide", page_title="Check identity document")
@@ -35,48 +36,31 @@ st.sidebar.write("## Upload and download :gear:")
 col1, col2 = st.columns(2)
 
 my_upload = st.sidebar.file_uploader("Upload an image", type=["pdf"])
+
 # Save the uploaded file to disk
 if my_upload is not None:
     # save the uploaded file to disk
     with open("uploaded_file.pdf", "wb") as buffer:
         shutil.copyfileobj(my_upload, buffer)
-        
+      
     displayPDF("uploaded_file.pdf")
-
-
-# Open a pdf file with fitz library
-def open_pdf(file):
-    # Open the file
-    doc = fitz.open(file)
-
-    # Get the first page
-    page = doc.loadPage(0)
-
-    # Get the page text
-    text = page.getText()
-
-    # Return the text
-    return text
 
 
 def call_api(file, api, api_key):
 
     # Encode the file in binary
     # binary_pdf = file.getvalue().encode('utf-8')
-    with open('test.pdf', "rb") as f:
-        file_content = f.read()
-
-    # The format of each instance should conform to the deployed model's prediction input schema.
-    encoded_content = base64.b64encode(file_content).decode("utf-8")
-    
+    # with open('test.pdf', "rb") as f:
+    #     file_content = f.read()
+   
 
     # Create a dictionary with the file     
     #data = {'file': binary_pdf}
-    data = {'file': encoded_content}
+    data = {'file': file}
 
 
     # Call the api with the file and the api key
-    r = requests.post(api, data=data, params={'key': api_key}, headers={'Content-Type': 'application/pdf',
+    r = requests.post(api, files=data, params={'key': api_key}, headers={'Content-Type': 'application/pdf',
                                                                         'Accept': '*/*',
                                                                         'Accept-Encoding': 'gzip, deflate, br',
                                                                         'Connection': 'keep-alive',
@@ -93,4 +77,37 @@ call_api_button = st.sidebar.button("Call the API")
 
 # Call the api when the button is clicked
 if call_api_button:
-    response = call_api(file=my_upload, api=api_url, api_key=api_key)
+    response = call_api(file=my_upload.read(), api=api_url, api_key=api_key)
+
+    # Display the response in a form with streamlit function st.form
+    # The form displays the response in text fields. 
+    # Fields extracted from response are [nome, cognome, tipo_documento, id_documento, data_scadenza, motivazione, documento_valido]
+    response = json.loads(response.text)
+    nome = response['nome']
+    cognome = response['cognome']
+    tipo_documento = response['tipo_documento']
+    id_documento = response['id_documento']
+    data_scadenza = response['data_scadenza']
+    motivazione = response['motivazione']
+    documento_valido = response['documento_valido']
+
+    # Display the response in a form with streamlit function st.form on the right column
+
+    with st.form(key='my_form'):
+        col2.write("## Response :mag_right:")
+        col2.write("### Name :bust_in_silhouette:")
+        col2.write(nome)
+        col2.write("### Surname :bust_in_silhouette:")
+        col2.write(cognome)
+        col2.write("### Document type :page_facing_up:")
+        col2.write(tipo_documento)
+        col2.write("### Document id :page_facing_up:")
+        col2.write(id_documento)
+        col2.write("### Expiration date :page_facing_up:")
+        col2.write(data_scadenza)
+        col2.write("### Motivation :page_facing_up:")
+        col2.write(motivazione)
+        col2.write("### Document valid :page_facing_up:")
+        col2.write(documento_valido)
+        submit_button = st.form_submit_button(label='OK')
+
